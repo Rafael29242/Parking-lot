eb37b8d0224f02d9c0d49aef7bb5af3e0e63d38c
Rafi Gomez <77979382+Rafael29242@users.noreply.github.com>
2021-04-07 19:01:27 -0400

    Add files via upload

---
+++ Ch7_Parking lot/ParkingLot.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch7_Parking_lot
+{
+    class ParkingLot
+    {
+
+        //Fields
+        private int LargeCar;
+        private int MediumCar;
+        private int SmallCar;
+
+        public ParkingLot(int setLarge = 1, int setMedium = 1, int setSmall = 1)
+        {
+            SetLargeCar(setLarge);
+            SetMediumCar(setMedium);
+            SetSmallCar(setSmall);
+        }
+
+        public void SetLargeCar(int Value)
+        {
+            LargeCar = Value;
+        }
+
+        public void SetMediumCar(int Value)
+        {
+            MediumCar = Value;
+        }
+
+        public void SetSmallCar(int Value)
+        {
+            SmallCar = Value;
+        }
+
+        public bool AddCar(string carType)
+        {
+            bool driveIn;
+            driveIn = true;
+
+            if(carType == "Large")
+            {
+                if(LargeCar > 0)
+                {
+                    LargeCar -= 1;
+                    driveIn = true;
+                }
+
+                else
+                {
+                    driveIn = false;
+                }
+            }
+
+            else if (carType == "Medium")
+            {
+                if(MediumCar > 0)
+                {
+                    MediumCar -= 1;
+                    driveIn = true;
+                }
+
+                else
+                {
+                    driveIn = false;
+                }
+            }
+
+            else if (carType == "Small")
+            {
+                if(SmallCar > 0)
+                {
+                    SmallCar -= 1;
+                    driveIn = true;
+                }
+
+                else
+                {
+                    driveIn = false;
+                }
+            }
+
+            return driveIn;
+
+        }
+
+        public int GetLarge()
+        {
+            return LargeCar;
+        }
+
+        public int GetMedium()
+        {
+            return MediumCar;
+        }
+
+        public int GetSmall()
+        {
+            return SmallCar;
+        }
+    }
+}
---
+++ Ch7_Parking lot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ch7_Parking_lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingLot CarParking = new ParkingLot();
+
+            CarParking.SetLargeCar(8);
+            CarParking.SetMediumCar(12);
+            CarParking.SetSmallCar(16);
+
+            Console.WriteLine($"The parking garage have {CarParking.GetLarge()} large parking spots, {CarParking.GetMedium()} medium sized parking spots, and {CarParking.GetSmall()} small parking spots.");
+
+        }
+    }
+}
